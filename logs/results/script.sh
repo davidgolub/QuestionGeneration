85b63b39ad9aeb3b3aef91ff2c2abcec7562c85d
David <golubd@stanford.edu>
2017-09-21 14:06:58 -0700

    Added json and script

--- logs/results/script.sh
+++ logs/results/script.sh
@@ -2,6 +2,10 @@
 echo "out of domain baseline" 
 python3 newsqa/evaluate.py newsqa/data_test.json answer_out_of_domain_baseline.json
 
+# Evaluate single model results (for steps 2k-10k)
+
+# Evaluate single model results + baseline (for steps 2k-10k)
+
 # Evaluate single-model result (44.5 F1)
 echo "A_(gen + Ner)"
 python3 newsqa/evaluate.py newsqa/data_test.json single_model.json 
@@ -13,3 +17,15 @@ python3 newsqa/evaluate.py newsqa/data_test.json double_model.json
 # Evaluate AOracle + Context
 echo "Answer oracle with context for question generation, single model"
 python3 newsqa/evaluate.py newsqa/data_test.json context_aoracle.json
+
+# Evaluate single model result of BiDAF finetuned on NewsQA
+echo "Single BiDAF model finetuned on NewsQA results"
+for num in 42 43 44 45 46 47 48 49; do
+    python3 newsqa/evaluate.py newsqa/data_test.json "single_model_results_${num}.json"
+done
+
+echo "Single BiDAF model finetuned on NewsQA ensembled w. baseline results"
+# Evaluate single model ensembled with baseline result of BiDAF finetuned on NewsQA
+for num in 42 43 44 45 46 47 48 49; do
+    python3 newsqa/evaluate.py newsqa/data_test.json "single_model_result_run_${num}_with_baseline.json"
+done
